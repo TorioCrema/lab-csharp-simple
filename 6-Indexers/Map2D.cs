49843d4c54f3d07052bbb6724d6ff506460ec2cc
Simone Zama <simone.zama@studio.unibo.it>
2021-12-12 13:07:58 +0100

    Improve GetHashCode and ToString method

--- 6-Indexers/Map2D.cs
+++ 6-Indexers/Map2D.cs
@@ -70,15 +70,13 @@ namespace Indexers
         /// <inheritdoc cref="object.GetHashCode"/>
         public override int GetHashCode()
         {
-            // TODO: improve
-            return base.GetHashCode();
+            return this.GetElements().GetHashCode();
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.ToString"/>
         public override string ToString()
         {
-            // TODO: improve
-            return base.ToString();
+            return this.GetElements().ToString();
         }
     }
 }
